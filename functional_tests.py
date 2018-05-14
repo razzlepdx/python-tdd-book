3cb420e6c484327c7013188f56f1cef4dbf0990a
razzlepdx <rwilson.or@gmail.com>
2018-05-14 12:54:26 -0700

    add changes to list item table before refactoring - first to-do can populate

--- functional_tests.py
+++ functional_tests.py
@@ -43,22 +43,28 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in the to-do list
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertEqual(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical.)
-
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         # The page updates again, and now it shows both items on her list.
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        self.fail('Finish the test!')
 
         # Edith wonders if the site will remember her list.  Then she sees that
         # the site has generated a unique URL for her -- there is some explanatory
